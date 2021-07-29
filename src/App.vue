6783301af3d8f108d6aedc056f1d247a1bb2fae6
Siji007 <pomolia007@gmail.com>
2021-07-29 19:19:05 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    
+    
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  
+  },
+ 
+}
+</script>
+
+<style>
+
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueTailwind from 'vue-tailwind'
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+    // Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+Vue.use(VueTailwind);
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Vuelidate from 'vuelidate'
+import App from './App.vue'
+import "@/assets/styles/main.css";
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+// import routes from './route'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+Vue.use(Vuelidate)
+Vue.config.productionTip = false
+
+// const router = new VueRouter({
+//     // routes,
+//     mode: 'history'
+// })
+new Vue({
+    render: h => h(App),
+    // router
+
+}).$mount('#app')
