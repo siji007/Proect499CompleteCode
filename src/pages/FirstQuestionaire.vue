b20424798e178588463580f1a37480d5bcac2871
therealsiji <55184842+siji007@users.noreply.github.com>
2021-12-03 11:10:45 +0100

    Update FirstQuestionaire.vue

--- src/pages/FirstQuestionaire.vue
+++ src/pages/FirstQuestionaire.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="mt-4">
-      <form action="" @submit.prevent="submit">
+      <form action="" @submit.prevent="onSubmit">
         <div v-show="showFirst">
           <div class="mt-4">
             <button class="btn btn-dark btn-sm"><p>Section 1 of 2</p></button>
@@ -31,8 +31,8 @@
                 <li >
                     <label>
                         <input
-                        style="border:none;" 
-                        v-model="email"  @blur="$v.email.$touch()" name=""  class="rounded-full p-2 border-success" type="email" >
+                        style="border:none;focus:none;" 
+                        v-model="email"  @blur="$v.email.$touch()" name=""  class="rounded-full p-2 border border-success" type="email" >
                         <!-- <span class="ml-2"></span> -->
                         <p class="p-1 text-danger font-serif" v-if="!$v.email.email"><i class=""></i>Please enter a valid email</p>
                         <p class="p-1 text-danger font-serif" v-if="!$v.email.required"><i class=""></i>Email is required</p>
@@ -50,11 +50,11 @@
                 <p class="font-bold mb-2">Age</p>
                 <li v-for="age in Ages" :key="age">
                     <label>
-                        <input v-model="ageError"  @input="$v.ageError.$touch()" name="age" max="1" class="" type="radio"  v-bind:value="age.id" v-bind:id="age.id">
+                        <input v-model="ageRef"  @input="$v.ageRef.$touch()" name="age" max="1" class="" type="radio"  v-bind:value="age.id" v-bind:id="age.id">
                         <span class="ml-2">{{ age.title }}</span>
                     </label>
                 </li>
-                <p class="text-danger font-serif" v-if="!$v.ageError.required">Please select a option</p>
+                <p class="text-danger font-serif" v-if="!$v.ageRef.required">Please select a option</p>
             </ul>
 
             <!-- <label for="" class="text-large font-bold">Age</label> <br />
@@ -75,11 +75,11 @@
                 <p class="font-bold mb-2">Gender</p>
                 <li v-for="gender in Genders" :key="gender">
                     <label v-bind:for="gender.id">
-                        <input v-model="genderError"  @input="$v.genderError.$touch()" name="gender" class="" type="radio"  v-bind:value="gender.id" v-bind:id="gender.id">
+                        <input v-model="genderRef"  @input="$v.genderRef.$touch()" name="gender" class="" type="radio"  v-bind:value="gender.id" v-bind:id="gender.id">
                         <span class="ml-2">{{ gender.title }}</span>
                     </label>
                 </li>
-                <p class="text-danger font-serif" v-if="!$v.genderError.required">Please select a option</p>
+                <p class="text-danger font-serif" v-if="!$v.genderRef.required">Please select a option</p>
             </ul>
             <!-- <label for="" class="text-large font-bold">Gender</label> <br />
             <input type="radio" required name="gender" id="male" />
@@ -93,15 +93,15 @@
             style="background-color: #8fe58e"
           >
             <ul class="-ml-8" style="list-style-type: none; " >
-                <p class="font-bold mb-2">Class</p>
-                <li v-for="classes in seniorClass" :key="classes">
-                    <label v-bind:for="classes.id">
-                        <input v-model="classError" @input="$v.classError.$touch()" class="" name="class" type="radio" v-bind:value="classes.id" v-bind:id="classes.id">
-                        <span class="ml-2">{{ classes.title }}</span>
-                    </label>
-                </li>
+              <p class="font-bold mb-2">Class</p>
+              <li v-for="classes in seniorClass" :key="classes">
+                  <label v-bind:for="classes.id">
+                    <input v-model="classRef" @input="$v.classRef.$touch()" class="" name="class" type="radio" v-bind:value="classes.id" v-bind:id="classes.id">
+                    <span class="ml-2">{{ classes.title }}</span>
+                  </label>
+              </li>
             </ul>
-            <p class="text-danger font-serif" v-if="!$v.classError.required">Please select a option</p>
+            <p class="text-danger font-serif" v-if="!$v.classRef.required">Please select a option</p>
             <!-- <label for="" class="text-large font-bold">Class</label> <br />
             <input type="radio" required name="class" id="ss1" />
             <span class="ml-2">SS1</span><br />
@@ -116,18 +116,17 @@
             class="form-group mt-4 p-4 rounded-lg"
             style="background-color: #8fe58e"
           >
-
             <ul class="-ml-8" style="list-style-type: none; " >
                 <p class="font-bold mb-2">Do you have access to information technology?</p>
                 <li v-for="access in AccessIt" :key="access">
-                    <div class="input" :class="{invalid: $v.accessItYnError.$error}">
+                    <div class="input" :class="{invalid: $v.accessItYnRef.$error}">
                     <label v-bind:for="access.id">
-                        <input v-model="accessItYnError" @input="$v.accessItYnError.$touch()" class="" name="access" type="radio" v-bind:value="access.id" v-bind:id="access.id">
+                        <input v-model="accessItYnRef" @input="$v.accessItYnRef.$touch()" class="" name="access" type="radio" v-bind:value="access.id" v-bind:id="access.id">
                         <span class="ml-2">{{ access.title }}</span>
                     </label>
                     </div>
                 </li>
-                <p class="text-danger font-serif" v-if="!$v.accessItYnError.required">Please select a option</p>
+                <p class="text-danger font-serif" v-if="!$v.accessItYnRef.required">Please select a option</p>
             </ul>
             <!-- <label for="" class="text-large font-bold"
               >Do you have access to information technology?</label
@@ -158,10 +157,17 @@
             <ul class="-ml-8" style="list-style-type: none; " >
                 <p class="font-bold mb-2">If Yes, what method accessing information technology?</p>
                 <li v-for="device in devices" :key="device">
+                  <div>
                     <label v-bind:for="device.id">
+
                       <input class=""  type="checkbox" v-model="device.checked" v-bind:value="device.id" v-bind:id="device.id">
                       <span class="ml-2">{{ device.title }}</span>
+
+                      <!-- <input class="" @input="$v.devicesRef.$touch()"  type="checkbox" v-model="devicesRef" v-bind:value="device.id" v-bind:id="device.id">
+                      <span class="ml-2">{{ device.title }}</span> -->
+                      
                     </label>
+                    </div>
                 </li>
             </ul>
           </div>
@@ -216,12 +222,26 @@ import {firstQuestionMixin} from '../firstQuestion'
 // import {required} from 'vuelidate/lib/validators'
 import SecondQuestionaire from "@/pages/SecondQuestionaire";
 export default {
-    mixins: [firstQuestionMixin],
-    components: {
-      SecondQuestionaire,
-    }
-  
-};
+  mixins: [firstQuestionMixin],
+  components: {
+    SecondQuestionaire,
+  },
+  methods:{
+  //  onSubmit() {
+  //       if (!this.$v.$invalid) {
+  //         const user = {
+  //           email: this.email,
+  //           //there could be a lot of props
+  //         };
+
+  //         console.log(user);
+
+  //         this.email = '';
+  //       }
+  //     }
+    },
+}
+
 </script>
 <style scoped>
 #divf {
