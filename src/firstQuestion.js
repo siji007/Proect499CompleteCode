47398e5d205923545845cda0a7050edf6242104f
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-12-08 12:28:51 +0100

    change data

--- src/firstQuestion.js
+++ src/firstQuestion.js
@@ -1,118 +1,120 @@
-import {required, email} from 'vuelidate/lib/validators'
+import { required, email } from 'vuelidate/lib/validators'
 export const firstQuestionMixin = {
-    data() {
-        return {
-          question4:'',
-          question1:'',
-          question2:'',
-          question3:'',
-          
-          // devicesRef: '',  
-          email: null,
-          showSecond: false,
-          showFirst: true,
-          submitButton: false,
-          Ages:[
-            {
-              "id": "l12",
-              "title": "Less than 12"
-            },
-            {
-            "id":"b1214",
-            "title": "12 - 14 years"
-            },
-            {
-            "id": "m14",
-            "title": "More than 14 years"
-            }
-          ],
-          Genders:[
-            {
-                "id": "male",
-                "title": "Male"
-            },
-            {
-                "id": "female",
-                "title": "Female"
-            },
-          ],
-          AccessIt:[
-            {   "id":"yes",
-                "title": "Yes"
-            },
-            {   "id":"no",
-                "title": "No"
-            },
-          ],
-          seniorClass:[
-            {
-                "id" : "ss1",
-                "title": "SS1"
-            },
-            {
-                "id" : "ss2",
-                "title": "SS2"
-            },
-            {
-                "id" : "ss3",
-                "title": "SS3"
-            },
-          ],
-          devices:[
-              
-            {
-            "id": "computer",
-            "title":"Computer",
-            "checked":true
-            },
-            {
-            "id": "television",
-            "title":"Television",
-            "checked":false
-            },
-            {
-            "id": "mobilePhone",
-            "title":"Mobile Phone",
-            "checked":false
-            },
-            {
-            "id": "videoGame",
-            "title":"Video Game",
-            "checked":false
-            },
-              
-          ]
-    
-        };
-      },
-      validations:{
-        email: {
-          required,email
+  data() {
+    return {
+      question4: '',
+      age: '',
+      gender: '',
+      clas: '',
+
+      // devicesRef: '',  
+      email: null,
+      showSecond: false,
+      showFirst: true,
+      submitButton: false,
+      Ages: [
+        {
+          "id": "l12",
+          "title": "Less than 12"
         },
-        question4:{
-          required:required
+        {
+          "id": "b1214",
+          "title": "12 - 14 years"
         },
-        question1:{
-          required:required
+        {
+          "id": "m14",
+          "title": "More than 14 years"
+        }
+      ],
+      Genders: [
+        {
+          "id": "male",
+          "title": "Male"
         },
-        question2:{
-          required:required
+        {
+          "id": "female",
+          "title": "Female"
         },
-        question3:{
-          required: required
+      ],
+      AccessIt: [
+        {
+          "id": "yes",
+          "title": "Yes"
         },
-        // devicesRef:{
-        //   required:required
-        // }
-      },
-      methods: {
-        submit() {
-          console.log("Pass");
+        {
+          "id": "no",
+          "title": "No"
         },
-        display() {
-          this.showSecond = !this.showSecond;
-          this.showFirst = !this.showFirst;
-          this.submitButton = !this.submitButton;
+      ],
+      seniorClas: [
+        {
+          "id": "ss1",
+          "title": "SS1"
         },
-      },
+        {
+          "id": "ss2",
+          "title": "SS2"
+        },
+        {
+          "id": "ss3",
+          "title": "SS3"
+        },
+      ],
+      devices: [
+
+        {
+          "id": "computer",
+          "title": "Computer",
+          "checked": true
+        },
+        {
+          "id": "television",
+          "title": "Television",
+          "checked": false
+        },
+        {
+          "id": "mobilePhone",
+          "title": "Mobile Phone",
+          "checked": false
+        },
+        {
+          "id": "videoGame",
+          "title": "Video Game",
+          "checked": false
+        },
+
+      ]
+
+    };
+  },
+  validations: {
+    email: {
+      required, email
+    },
+    question4: {
+      required: required
+    },
+    age: {
+      required: required
+    },
+    gender: {
+      required: required
+    },
+    clas: {
+      required: required
+    },
+    // devicesRef:{
+    //   required:required
+    // }
+  },
+  methods: {
+    submit() {
+      console.log("Pass");
+    },
+    display() {
+      this.showSecond = !this.showSecond;
+      this.showFirst = !this.showFirst;
+      this.submitButton = !this.submitButton;
+    },
+  },
 }
